e04e29095d63e974f6854b466b4f20f861997c13
mustahsin2000 <mustahsin152415@gmail.com>
2023-08-09 21:56:09 +0600

    academic semester

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import cors from 'cors';
-import { UserRoutes } from './app/modules/user/user.route';
+// import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import express, { Application } from 'express';
+// import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import routes from './app/routes';
+
 // import ApiError from './errors/ApiError'
 const app: Application = express();
 
@@ -14,7 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 //application routes
 // eslint-disable-next-line no-console
 console.log(app.get('env'));
-app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+
+app.use('/api/v1', routes);
 
 // Testing
 // app.get('/', async (_req: Request, res: Response, next: NextFunction) => {
